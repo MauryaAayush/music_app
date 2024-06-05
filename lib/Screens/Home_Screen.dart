4143bb535ac35b091fc2e162ad8f62e631dbbfb1
Admin <maayush235@gmail.com>
2024-06-05 22:41:42 +0530

    Radhey Radhey

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -5,6 +5,12 @@ class HomeScreen extends  StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: Icon(Icons.menu),
+        title: Text('Music'),
+      ),
+    );
   }
 }
