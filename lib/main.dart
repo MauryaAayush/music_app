c15e4971554b508d10f12803735b29317c9ad800
Admin <maayush235@gmail.com>
2024-06-05 14:37:37 +0530

    Radhey Radhey

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Home_Screen.dart';
+
+void main()
+{
+  runApp(const MusicApp());
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
