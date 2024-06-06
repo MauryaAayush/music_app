bf8fca614f222a6e7cf13b11afe704cefb32d085
Admin <maayush235@gmail.com>
2024-06-06 07:22:46 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'Screens/Home_Screen.dart';
-
 void main()
 {
   runApp(const MusicApp());
@@ -15,6 +13,38 @@ class MusicApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+    );
+  }
+}
+
+
+
+
+class HomeScreen extends  StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          leading: IconButton(onPressed: () {
+            setState(() {
+              Scaffold.of(context).openDrawer();
+            });
+          }, icon:  Icon(Icons.horizontal_split_rounded,),
+            color: Theme.of(context).iconTheme.color,
+          ),
+          title: Text('Music'),
+        ),
+        drawer:  Drawer(
+
+        )
     );
   }
 }
