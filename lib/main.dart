8e7707964463caea9fef4ad9c1382b70a1a1c4a8
Admin <maayush235@gmail.com>
2024-06-08 12:54:01 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/Providers/page_provider.dart';
 import 'package:music_app/Providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -9,6 +10,9 @@ void main() {
     MultiProvider(providers: [
       ChangeNotifierProvider(
         create: (context) => ThemeProvider(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => PageProvider(),
       )
     ], child: const MusicApp()),
   );
@@ -27,5 +31,3 @@ class MusicApp extends StatelessWidget {
     );
   }
 }
-
-
