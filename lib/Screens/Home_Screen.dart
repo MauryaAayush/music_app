4d9b1dce36012b23677dc928904f949abff67291
Admin <maayush235@gmail.com>
2024-06-08 12:56:51 +0530

    Radhey Radhey

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -2,6 +2,7 @@ import 'package:bottom_bar/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../Components/drawer_code.dart';
 import '../Providers/theme_provider.dart';
 import '../Providers/page_provider.dart';
 
@@ -44,131 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      drawer: Drawer(
-        width: 350,
-        child: CustomScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: false,
-          slivers: [
-            SliverAppBar(
-              centerTitle: true,
-              backgroundColor: Colors.transparent,
-              automaticallyImplyLeading: false,
-              stretch: false,
-              elevation: 0,
-              expandedHeight: MediaQuery.of(context).size.height * 0.2,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: RichText(
-                  text: TextSpan(
-                    text: 'Music Hole\n',
-                    style: const TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'version 2.0',
-                        style: const TextStyle(
-                          fontSize: 7.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.end,
-                ),
-                titlePadding: const EdgeInsets.only(bottom: 40.0),
-                background: ShaderMask(
-                  shaderCallback: (rect) {
-                    return LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.black.withOpacity(0.8),
-                        Colors.black.withOpacity(0.1),
-                      ],
-                    ).createShader(
-                      Rect.fromLTRB(0, 0, rect.width, rect.height),
-                    );
-                  },
-                  blendMode: BlendMode.dstIn,
-                  child: Image(
-                    fit: BoxFit.cover,
-                    alignment: Alignment.topCenter,
-                    image: AssetImage(
-                      Theme.of(context).brightness == Brightness.dark
-                          ? 'assets/header-dark.jpg'
-                          : 'assets/header.jpg',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  ListTile(
-                    leading: Icon(
-                      Icons.home,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      'Home',
-                      style: TextStyle(
-                        color: Colors.teal,
-                      ),
-                    ),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.folder),
-                    title: const Text('My Music'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.download_done_sharp),
-                    title: const Text('Download'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.playlist_play),
-                    title: const Text('Playlists'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.settings),
-                    title: const Text('Settings'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.info_outline),
-                    title: const Text('About'),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
-            const SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                children: <Widget>[
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(5, 30, 5, 20),
-                    child: Center(
-                      child: Text(
-                        'Made with ❤️ by Aayush Maurya',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      drawer: DrawerScreen(),
       body: PageView(
         controller: _pageController,
         children: [
@@ -217,3 +94,5 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
