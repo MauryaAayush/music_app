bb259ad83e515fa4d918123ccb32c37559c97f86
Admin <maayush235@gmail.com>
2024-06-13 23:07:32 +0530

    Radhey radhey

--- lib/Components/Home Screen Code.dart
+++ lib/Components/Home Screen Code.dart
@@ -1,74 +1,38 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../Providers/theme_provider.dart';
-import '../model/Music_List.dart';
 import 'SliverAppBarForGeading.dart';
 import 'SliverAppBarForSearch.dart';
 import 'carousal_Column.dart';
 
-class HomeScreenCode extends StatefulWidget {
+class HomeScreenCode extends StatelessWidget {
   const HomeScreenCode({
     super.key,
-    required this.scrollController,
-    required this.containerWidth,
+    required ScrollController scrollController,
+    required double containerWidth,
     required this.themeProvider,
-  });
+  }) : _scrollController = scrollController, _containerWidth = containerWidth;
 
-  final ScrollController scrollController;
-  final double containerWidth;
+  final ScrollController _scrollController;
+  final double _containerWidth;
   final ThemeProvider themeProvider;
-
-  @override
-  _HomeScreenCodeState createState() => _HomeScreenCodeState();
-}
-
-class _HomeScreenCodeState extends State<HomeScreenCode> {
-  late AudioPlayer _audioPlayer;
-  String? _currentSong;
-
-  @override
-  void initState() {
-    super.initState();
-    _audioPlayer = AudioPlayer();
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-
-  void _playSong(String song) async {
-    if (_currentSong == song) {
-      await _audioPlayer.stop();
-      setState(() {
-        _currentSong = null;
-      });
-    } else {
-      await _audioPlayer.play(AssetSource(song));
-      setState(() {
-        _currentSong = song;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         NestedScrollView(
-          controller: widget.scrollController,
+          controller: _scrollController,
           physics: const BouncingScrollPhysics(),
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return <Widget>[
               const SliverForGreading(),
               SliverForSearch(
-                  scrollController: widget.scrollController,
-                  containerWidth: widget.containerWidth),
+                  scrollController: _scrollController,
+                  containerWidth: _containerWidth),
             ];
           },
           body: SingleChildScrollView(
@@ -76,6 +40,8 @@ class _HomeScreenCodeState extends State<HomeScreenCode> {
             scrollDirection: Axis.vertical,
             child: Column(
               children: [
+
+                // Heading of type of songs
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                   child: Row(
@@ -89,10 +55,11 @@ class _HomeScreenCodeState extends State<HomeScreenCode> {
                           color: Colors.tealAccent,
                         ),
                       ),
-                      const Icon(Icons.chevron_right),
+                      Icon(Icons.chevron_right),
                     ],
                   ),
                 ),
+                // Add Playlist Widget here
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                   child: Container(
@@ -102,7 +69,8 @@ class _HomeScreenCodeState extends State<HomeScreenCode> {
                         color: Colors.blue,
                         borderRadius: BorderRadius.circular(20)
                     ),
-                    child: const Center(child: Text('Favorite Songs')),
+                    // Replace with your playlist widget
+                    child: Center(child: Text('Favorite Songs')),
                   ),
                 ),
                 Padding(
@@ -118,56 +86,297 @@ class _HomeScreenCodeState extends State<HomeScreenCode> {
                           color: Colors.tealAccent,
                         ),
                       ),
-                      const Icon(Icons.chevron_right),
+                      Icon(Icons.chevron_right),
                     ],
                   ),
                 ),
-                CarouselSlider(
-                  items: const [
-                    Carousal_Column(),
-                    Carousal_Column(),
-                    Carousal_Column(),
-                    Carousal_Column(),
-                  ],
-                  options: CarouselOptions(
-                    height: 310,
-                    padEnds: false,
-                    viewportFraction: 0.85,
-                    enableInfiniteScroll: false,
-                    autoPlay: false,
+                // Add Last Session Widgets here
+
+                CarouselSlider(items: const [
+                  Carousal_Column(),
+                  Carousal_Column(),
+                  Carousal_Column(),
+                  Carousal_Column(),
+
+                ], options: CarouselOptions(
+                  height: 310,
+                  padEnds: false,
+                  viewportFraction:
+                  0.85, // Adjust this value for visibility from the right
+                  enableInfiniteScroll: false,
+                  autoPlay: false,
+                )),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Trending Now',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
                   ),
                 ),
-                ..._buildSection('Trending Now', 5),
-                ..._buildSection('Top Charts', 5),
-                ..._buildSection('New Released', 5),
-                ..._buildSection('Radio Stations', 5, isCircle: true),
-                ..._buildSection('Recommended Artist', 5, isCircle: true),
-                ..._buildSection('Devotional', 5),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Top Charts',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'New Released',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Radio Stations',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+                        width: 170,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue,
+
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Recommended Artist',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+                        width: 170,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue,
+
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Devotional',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 180,
+
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 8.0,bottom: 8),
+                      child: Container(
+
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        // Replace with your playlist widget
+                        child: Center(child: Text('Favorite Songs')),
+                      ),
+                    ),),
+                ),
+
               ],
             ),
           ),
         ),
         Builder(
           builder: (context) => Padding(
-            padding: const EdgeInsets.only(top: 8.0, left: 4.0, right: 4.0),
+            padding: const EdgeInsets.only(
+                top: 8.0, left: 4.0, right: 4.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
               children: [
                 Transform.rotate(
                   angle: 22 / 7 * 2,
                   child: IconButton(
-                    icon: const Icon(Icons.horizontal_split_rounded),
+                    icon: const Icon(
+                        Icons.horizontal_split_rounded),
                     onPressed: () {
                       Scaffold.of(context).openDrawer();
                     },
-                    tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                    tooltip: MaterialLocalizations.of(context)
+                        .openAppDrawerTooltip,
                   ),
                 ),
                 IconButton(
-                  icon: Icon(widget.themeProvider.isDarkMode
+                  icon: Icon(themeProvider.isDarkMode
                       ? Icons.wb_sunny
                       : Icons.nights_stay),
-                  onPressed: () => widget.themeProvider.toggleTheme(),
+                  onPressed: () =>
+                      themeProvider.toggleTheme(),
                 ),
               ],
             ),
@@ -176,52 +385,4 @@ class _HomeScreenCodeState extends State<HomeScreenCode> {
       ],
     );
   }
-
-  List<Widget> _buildSection(String title, int count, {bool isCircle = false}) {
-    return [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              title,
-              style: GoogleFonts.roboto(
-                fontSize: 18,
-                letterSpacing: 1,
-                fontWeight: FontWeight.bold,
-                color: Colors.tealAccent,
-              ),
-            ),
-            const Icon(Icons.chevron_right),
-          ],
-        ),
-      ),
-      SizedBox(
-        height: 180,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemCount: count,
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () => _playSong(music1[index]),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10.0, top: 8.0, bottom: 8.0),
-              child: Container(
-                width: 170,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: isCircle ? null : BorderRadius.circular(20),
-                  shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
-                ),
-                child: Center(child: Text('Favorite Songs')),
-              ),
-            ),
-          ),
-        ),
-      ),
-    ];
-  }
 }
-
-
