8b4bd83ec0d9df77074d031ffacecf1c6e6b8b78
Admin <maayush235@gmail.com>
2024-06-15 16:11:49 +0530

    Radhey radhey

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -34,27 +34,8 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<  ThemeProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: _searchController,
-          onSubmitted: (value) {
-            Provider.of<AudioPlayerProvider>(context, listen: false)
-                .searchSongs(value);
 
-            if (value.isNotEmpty) {
-              setState(() {
-                recentSearches.add(value);
-                _searchController
-                    .clear(); // Clear the text field after submission
-              });
-            }
-          },
-          decoration: InputDecoration(
-            hintText: 'Songs, albums or artists',
-            border: InputBorder.none,
-          ),
-        ),
-      ),
+      
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -68,92 +49,117 @@ class _SearchScreenState extends State<SearchScreen> {
 
 
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Consumer<AudioPlayerProvider>(
-                    builder: (context, provider, child) {
-                  if (provider.isLoading) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.white,
-                      ),
-                    );
-                  }
-
-                  // Check if songs are empty
-                  if (provider.songs.isEmpty) {
-                    return Center(
-                      child: Text('No songs found'),
-                    );
-                  }
-
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount:
-                          Provider.of<AudioPlayerProvider>(context, listen: true)
-                              .songs
-                              .length,
-                      itemBuilder: (context, index) {
-                        Song song = Provider.of<AudioPlayerProvider>(context,
-                                listen: true)
-                            .songs[index];
-                        return ListTile(
-                          leading: Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(song.image)),
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                          title: Text(song.song, style: TextStyle(fontSize: 20)),
-                          subtitle: Text(
-                            song.singers,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
+          
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: _searchController,
+                    onSubmitted: (value) {
+                      Provider.of<AudioPlayerProvider>(context, listen: false)
+                          .searchSongs(value);
+            
+                      if (value.isNotEmpty) {
+                        setState(() {
+                          recentSearches.add(value);
+                          _searchController
+                              .clear(); // Clear the text field after submission
+                        });
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Songs, albums or artists',
+                      border: InputBorder.none,
+                    ),
+                  ),
+            
+            
+                  Consumer<AudioPlayerProvider>(
+                      builder: (context, provider, child) {
+                    if (provider.isLoading) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
+                      );
+                    }
+            
+                    // Check if songs are empty
+                    if (provider.songs.isEmpty) {
+                      return Center(
+                        child: Text('No songs found'),
+                      );
+                    }
+            
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount:
+                            Provider.of<AudioPlayerProvider>(context, listen: true)
+                                .songs
+                                .length,
+                        itemBuilder: (context, index) {
+                          Song song = Provider.of<AudioPlayerProvider>(context,
+                                  listen: true)
+                              .songs[index];
+                          return ListTile(
+                            leading: Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(song.image)),
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            title: Text(song.song, style: TextStyle(fontSize: 20)),
+                            subtitle: Text(
+                              song.singers,
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          );
+                        });
+                  }),
+            
+                    Wrap(
+                      spacing: 8.0,
+                      children: recentSearches.map((search) {
+                        return Chip(
+                          label: Text(search),
+                          deleteIcon: Icon(Icons.cancel),
+                          onDeleted: () {
+                            setState(() {
+                              recentSearches.remove(search);
+                            });
+                          },
                         );
-                      });
-                }),
-
-                  Wrap(
-                    spacing: 8.0,
-                    children: recentSearches.map((search) {
-                      return Chip(
-                        label: Text(search),
-                        deleteIcon: Icon(Icons.cancel),
-                        onDeleted: () {
-                          setState(() {
-                            recentSearches.remove(search);
-                          });
-                        },
-                      );
-                    }).toList(),
+                      }).toList(),
+                    ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Trending Search',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal,
+                    ),
                   ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Trending Search',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal,
-                  ),
-                ),
-                // Show trending searches only if not searching
-                  Wrap(
-                    spacing: 8.0,
-                    children: trendingSearches.map((search) {
-                      return Chip(
-                        label: Text(search),
-                      );
-                    }).toList(),
-                  ),
-                // Show search results if searching
-
-              ],
+                  // Show trending searches only if not searching
+                    Wrap(
+                      spacing: 8.0,
+                      children: trendingSearches.map((search) {
+                        return Chip(
+                          label: Text(search),
+                        );
+                      }).toList(),
+                    ),
+                  // Show search results if searching
+            
+                ],
+              ),
             ),
           ),
         ),
